ad3ec66aa1a9474cc948a6b06c6f57f711faf39f
juanhapes <juanhapes@gmail.com>
2019-10-25 17:21:34 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { EventListenerTest } = require('./lib');
+
+module.exports = EventListenerTest;
---
+++ tests/event-listener-test-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// const assert = require('assert');
+
+// const EventListenerTest = require('./../index');
+
+// const EventListenerTestError = require('./../lib/event-listener-test-error')
+
+describe('EventListenerTest', () => {
+
+	// your tests here...
+
+});
